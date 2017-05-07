666b4ca76275366f7477a3c7f54c0dbe4857d613
Frank DuBose <frankdubose@Franks-MacBook-Air-3.local>
2017-05-06 20:02:58 -0400

    adding files

---
+++ dubose.github.io/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<!--
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<!-- script from LinkedIn -->
+	<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+	<head>
+		<title>DuBose the Designer</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<a href="index.html" class="logo"><strong>DuBose</strong> <span>the Designer</span></a>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu - This needs to be un-commented as I complete more webpages-->
+					<nav id="menu">
+						<ul class="links">
+							<li><a href="index.html">Home</a></li>
+							<li><a href="documents/web_resume_3_27_17.pdf">Resume</a></li>
+							<li><a href="engineering.html">Engineering Projects</a></li>
+                                                        <li><a href="writing.html">Writing Samples</a></li>
+                                                        <li><a href="work.html">Work Experience</a></li>
+                                                        <li><a href="art.html">Art Portfolio</a></li>
+                                                        <li><a href="dance.html">Dance</a></li>
+                                                        <!--<li><a href="espanol.html">Español</a></li>-->
+						</ul>
+						<ul class="links">
+							<li><h3><em>Coming Soon</em></h3></li>
+							<!--<li>Engineering Projects</li>-->
+							<!--<li>Writing Samples</li>-->
+							<!--<li>Work Experience</li>-->
+							<!--<li>Art Portfolio</li>-->
+							<!--<li>Dance</li>-->
+							<li>Español</li>
+						</ul>
+						<ul class="actions vertical">
+							<!--<li><a href="#" class="button special fit">Get Started</a></li>-->
+							<!--<li><a href="#" class="button fit">Log In</a></li>-->
+						</ul>
+					</nav>
+
